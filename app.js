3fbac6f565a7f07fea68e013ff272b8b959f767c
gary909 <garywhite61@hotmail.com>
2022-08-24 13:10:10 +0200

    code working

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 function isTriangle(a, b, c) {
-    return false;
+    let myArr = [a, b, c]; // put into an array to sort
+    myArr.sort(function (a, b) { // sort in ascending
+        return a - b;
+    }); // if the 1st two nums are greater than the last..
+    if (myArr[0] + myArr[1] > myArr[2]) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 console.log(isTriangle(1, 2, 2)); // true
 console.log(isTriangle(7, 2, 2)); // false
+console.log(isTriangle(7, 6, 3)); // true
+console.log(isTriangle(9, 8, 8)); // true
+console.log(isTriangle(6, 3, 4)); // true
